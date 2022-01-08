02b4c7330ce82d855a7d01eb7a8a6d374c5629bf
Student-Nathan <94365971+Student-Nathan@users.noreply.github.com>
2022-01-09 00:28:22 +0100

    Add files via upload

--- database.php
+++ database.php
@@ -5,12 +5,22 @@ function connectToDatabase() {
     $Connection = null;
 
     mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT); // Set MySQLi to throw exceptions
-    try {
-        $Connection = mysqli_connect("localhost", "root", "", "nerdygadgets");
-        mysqli_set_charset($Connection, 'latin1');
-        $DatabaseAvailable = true;
-    } catch (mysqli_sql_exception $e) {
-        $DatabaseAvailable = false;
+    if(isset($_SESSION["UserLogin"])) {
+        try {
+            $Connection = mysqli_connect("localhost", "klant", "betalendeBezoeker", "nerdygadgets");
+            mysqli_set_charset($Connection, 'latin1');
+            $DatabaseAvailable = true;
+        } catch (mysqli_sql_exception $e) {
+            $DatabaseAvailable = false;
+        }
+    }else{
+        try {
+            $Connection = mysqli_connect("localhost", "bezoeker", "bezoeker", "nerdygadgets");
+            mysqli_set_charset($Connection, 'latin1');
+            $DatabaseAvailable = true;
+        } catch (mysqli_sql_exception $e) {
+            $DatabaseAvailable = false;
+        }
     }
     if (!$DatabaseAvailable) {
         ?><h2>Website wordt op dit moment onderhouden.</h2><?php
